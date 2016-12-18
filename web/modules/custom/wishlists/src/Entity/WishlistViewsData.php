16dd942ac46254a7aa7eb6068ab0202c52395c68
Pieter Frenssen <pieter@frenssen.be>
2016-12-18 22:10:32 +0100

    Revert "Remove incorrect views data implementation. The default from core works fine."
    We should still keep the file even though we are not overriding it any more.
    Removing it entirely will cause the entity type to be ignored by Views.
    
    This partially reverts commit 5b945098e4e6149a9774ef12f19b772227829a75.

---
+++ web/modules/custom/wishlists/src/Entity/WishlistViewsData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\wishlists\Entity;
+
+use Drupal\views\EntityViewsData;
+use Drupal\views\EntityViewsDataInterface;
+
+/**
+ * Provides Views data for Wishlist entities.
+ */
+class WishlistViewsData extends EntityViewsData implements EntityViewsDataInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewsData() {
+    $data = parent::getViewsData();
+
+
+    return $data;
+  }
+
+}
